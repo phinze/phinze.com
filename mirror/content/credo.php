ec258cb63d3f9ec93dbbcc6a2e5995da34e7680c
Paul Hinze <paul.t.hinze@gmail.com>
2007-05-17 16:58:21 +0000

    Major overhaul. Up is down.  Random colors.
    git-svn-id: http://svn.phinze.com/web/mscs/trunk@226 33c0dc5c-f109-0410-8f75-9c819b2c7d68

--- mirror/content/credo.php
+++ mirror/content/credo.php
@@ -1,2 +1,2 @@
-<?php $pagetitle="I believe..."; ?>
+<?php $pagetitle="philosophy"; ?>
 <p>...in curiosity; in learning; in teaching; in listening; in good music; in rhythm; in humanity; in nature; in doing things right; in taking your time; in coding conventions; in web standards; in structure; in thinking things through; in humor; in balance; in purple; in source control; in dreams; in conversation; in vim; in love; in bootloaders; in backups; in hugs; in imperfection, in discovery, in you.</p>
--- mirror/layouts/default.php
+++ mirror/layouts/default.php
@@ -2,48 +2,55 @@
 <html>
 <head>
     <title>Paul Hinze, Marquette University COSC Student</title>
-    <link rel="stylesheet" href="http://mscs.mu.edu/~phinze/css/nn4.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $this->getHomeUrl(); ?>css/nn4.css" type="text/css" />
     <style type="text/css">
-    @import url("http://mscs.mu.edu/~phinze/css/typography.css");
-    @import url("http://mscs.mu.edu/~phinze/css/design.css");
-    @import url("http://mscs.mu.edu/~phinze/css/layout.css");
+	@import url("<?php echo $this->getHomeUrl(); ?>css/typography.css");
+    @import url("<?php echo $this->getHomeUrl(); ?>css/design.css");
+    @import url("<?php echo $this->getHomeUrl(); ?>css/layout.css");
+	<?php
+		$chars = array( 'c', '6', '3' );
+		shuffle( $chars );
+		$color = '#' . implode( '', $chars );
+	?>
+	#titlebar { background-color: <?php echo $color; ?>; }
+	a, a:visited, a:active { color: <?php echo $color; ?>; }
+	#nav a:hover { color: <?php echo $color; ?>; } 
     </style>
     <!--[if IE]>
-    <link rel="stylesheet" type="text/css" href="http://mscs.mu.edu/~phinze/css/ie-all.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo $this->getHomeUrl(); ?>css/ie-all.css" />
     <![endif]-->
 
     <!--[if lte IE 6]>
-    <link rel="stylesheet" type="text/css" href="http://mscs.mu.edu/~phinze/css/ie-lte6.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo $this->getHomeUrl(); ?>css/ie-lte6.css" />
     <![endif]-->
 </head>
 
 <body>
 <div id="wrapper">
-<div id="content">
-<h1><?php $this->displayTitle(); ?></h1>
-<?php $this->displayContent(); ?>
-</div>
-</div>
 <div id="footer">
-	<div id="nav">
-	<ul>
-		<li><a style="width: 9em;" href="/~phinze/">home</a></li>
-		<li><a style="width: 11em;" href="/~phinze/credo/">credo</a></li>
-		<li><a style="width: 13em;" href="/~phinze/projects/">projects</a></li>
-		<li><a style="width: 15em;" href="/~phinze/academics/">academics</a></li>
-	</ul>
-	</div>
+	<div id="qotd">&ldquo;<q>Computers are useless, they can only give you answers.</q>&rdquo; <cite>Picasso</cite></div>
 	<div id="titlebar">
-		<img src="http://mscs.mu.edu/~phinze/img/head.png" width="240" height="75" alt="head" />
+		<img src="<?php echo $this->getHomeUrl(); ?>img/head.png" width="240" height="75" alt="head" />
 		<!--Creative Commons License-->
 		<div id="cc_license">
 		<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.5/" title="This website is licensed under the Creative Commons License"><img alt="Creative Commons License" style="border-width: 0" src="http://creativecommons.org/images/public/somerights20.png"/></a>
 		</div>
 		<!--/Creative Commons License-->
-		<h1>~phinze</h1>
+		<h1>~phinze/<?php echo strtolower( $this->contentTitle ); ?></h1>
 	</div>
-	<div id="qotd">&ldquo;<q>Computers are useless, they can only give you answers.</q>&rdquo; <cite>Picasso</cite></div>
+	<div id="nav">
+	<ul>
+		<li><a style="width: 14em;" href="<?php echo $this->getHomeUrl(); ?>">home</a></li>
+		<li><a style="width: 12em;" href="<?php echo $this->getHomeUrl(); ?>projects/">projects</a></li>
+		<li><a style="width: 10em;" href="<?php echo $this->getHomeUrl(); ?>philosophy/">philosophy</a></li>
+		<li><a style="width: 8em;" href="<?php echo $this->getHomeUrl(); ?>academics/">academics</a></li>
+	</ul>
+	</div>
 </div>
+<div id="content">
+<?php $this->displayContent(); ?>
+</div> <!-- content -->
+</div> <!-- wrapper -->
 <!--
 <rdf:RDF xmlns="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
     <Work rdf:about="">
