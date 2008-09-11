eba274b73b86c0efd20bd0ae878034dc38b6122e
Paul Hinze <paul.t.hinze@gmail.com>
2008-09-11 20:15:42 +0000

    - adding geshi - starting imageareas
    git-svn-id: http://svn.phinze.com/web/phinze.com/trunk@273 33c0dc5c-f109-0410-8f75-9c819b2c7d68

---
+++ mirror/content/projects/imageareas.php
@@ -0,0 +1,13 @@
+<?php $pagetitle="proj/ia";
+$linkUrl = $this->getHomeUrl() . 'content/projects/';
+?>
+<h2>imageareas</h2>
+<p><em>Flipping bits instead of burgers.</em></p>
+
+<a style="display: block; float: right;" href="<?php echo $linkUrl; ?>img/gallery.png">
+	<img src="<?php echo $linkUrl; ?>img/gallery.png" style="width: 200px"/>
+</a>
+<p>Having had a job every summer doing something computer-related for some 
+university department every year I was in college, I realized in Spring of 2008 
+that school was going to end and I was going to be a jobless graduate.</p>
+
